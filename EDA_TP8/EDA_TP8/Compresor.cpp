b0164c0bd5381ce48f7287fefa427a7e3ce580ab
taomasgonzalez <togonzalez@itba.edu.ar>
2018-05-14 20:32:34 -0300

    Cambios en el compressor

--- EDA_TP8/EDA_TP8/Compresor.cpp
+++ EDA_TP8/EDA_TP8/Compresor.cpp
@@ -1,67 +1,80 @@
 #include "Compresor.h"
+#include <limits>
+#include <string>
 
 
 
-Compresor::Compresor(uint threshold, Compress_type compress_mode)
+Compresor::Compresor(FILE * file_stream)
 {
-	this->threshold = threshold;
-	this->compress_mode = compress_mode;
+	change_target_file(FILE * file_stream);
 }
-
 
 Compresor::~Compresor()
 {
-}
-
-void Compresor::compress() {
 
 }
 
+void Compresor::change_target_file(FILE * file_stream) {
+	to_write_file = file_stream;
+}
+void Compresor::compress(unsigned int w, unsigned int h, char out_lineal[], unsigned int threshold) {
+	
+	char** matrix = new char*[h];
+	for (int i = 0; i < h; ++i)
+		matrix[i] = new char[w];			//creo una matriz de char para facilitar la lectura y escritura de la imagen al comprimir.
 
+	array_to_matrix(out_lineal, h*w,matrix, w);
+	rec_comp(w, h, matrix, 0, 0, threshold);
 
-//Hice threshold dato miembro, menos villero
-void Compresor::rec_comp(unsigned int w, unsigned int h, char ** out, unsigned int init_x, unsigned int init_y) {
-		
-	uint puntaje = give_me_the_score( w,   h,  out,   init_x,   init_y);
-
-	if (puntaje >= this->threshold) {
-			unsigned int new_w_izq = w / 2;
-			unsigned int new_w_der = w / 2;
-			unsigned int new_h_hi = h / 2;
-			unsigned int new_h_lo = h / 2;
-
-			if (w % 2)
-				new_w_der++;
-			if (h % 2)
-				new_h_lo++;
-			//*-
-			//--
-			rec_comp(new_w_izq, new_h_hi, out, init_x, init_y);
-			//-*
-			//--
-			rec_comp(new_w_der, new_h_hi, out, init_x, init_y + new_w_izq);
-			//--
-			//*-
-			rec_comp(new_w_izq, new_h_lo, out, init_x + new_h_hi, init_y);
-			//--
-			//-*
-			rec_comp(new_w_der, new_h_lo, out, init_x + new_h_hi, init_y + new_w_izq);
-
-		}
-		else {
-			//hacer esto una función que llame a promedio y fputchar
-			char * prom = new char[4];
-			prom = promedio(w, h, out, init_x, init_y);
-			//fputc
-			delete prom;
-		}
+	for (int i = 0; i < h; ++i)
+		delete[] matrix[i];
+	delete[] matrix;						//borro la matriz para que no haya memory leaks
+}
+void Compresor::decompress() {
 
 }
 
-//Te tira error xq estas accediendo a cosas que no son stati desde un metodo static, el compilador no puede aseguar que esas cosas existan
-char* Compresor::promedio(unsigned int w, unsigned int h, char ** out, unsigned int init_x, unsigned int init_y) {
+void Compresor::rec_comp(unsigned int w, unsigned int h, char ** out, unsigned int init_x, unsigned int init_y, unsigned int threshold) {
 
-	char colores_prom[4];
+	unsigned int punt = puntaje(w, h, out, init_x, init_y);		//obtengo el puntaje del cuadrado en particular
+
+	if (punt >= threshold) {
+		fputc('B', to_write_file);
+
+		unsigned int new_w_izq = w / 2;
+		unsigned int new_w_der = w / 2;
+		unsigned int new_h_hi = h / 2;
+		unsigned int new_h_lo = h / 2;
+
+		if (w % 2)			//caso en que haya una cantidad impar de pixeles para el ancho
+			new_w_der++;
+		if (h % 2)			//caso en que haya una cantidad impar de pixeles para la altura
+			new_h_lo++;
+		//*-
+		//--
+		rec_comp(new_w_izq, new_h_hi, out, init_x, init_y, threshold);
+		//-*
+		//--
+		rec_comp(new_w_der, new_h_hi, out, init_x, init_y + new_w_izq, threshold);
+		//--
+		//*-
+		rec_comp(new_w_izq, new_h_lo, out, init_x + new_h_hi, init_y, threshold);
+		//--
+		//-*
+		rec_comp(new_w_der, new_h_lo, out, init_x + new_h_hi, init_y + new_w_izq, threshold);
+
+	}
+	else {
+		char prom[4];
+		promedio(prom, w, h, out, init_x, init_y);
+
+		std::string str = std::string("N ") + std::to_string(prom[0]) + " " + std::to_string(prom[1]) + " " + std::to_string(prom[2]);
+		fputs(str.c_str(), to_write_file);
+	}
+}
+
+void Compresor::promedio(char colores_prom[4], unsigned int w, unsigned int h, char ** out, unsigned int init_x, unsigned int init_y) {
+
 	unsigned int sum_r = 0;
 	unsigned int sum_g = 0;
 	unsigned int sum_b = 0;
@@ -81,47 +94,50 @@ char* Compresor::promedio(unsigned int w, unsigned int h, char ** out, unsigned 
 	colores_prom[2] = sum_b / (double)num_elements;
 	colores_prom[3] = 0xff;
 
-	return colores_prom;
 }
+unsigned int Compresor::puntaje(unsigned int w, unsigned int h, char **out, unsigned int init_x, unsigned int init_y) {
 
+	unsigned int max_R = 0;				
+	unsigned int min_R = UINT_MAX;
+	unsigned int max_G = 0;
+	unsigned int min_G = UINT_MAX;
+	unsigned int max_B = 0;
+	unsigned int min_B = UINT_MAX;
 
-uint Compresor::give_me_the_score(uint  w, uint  h, char ** out, uint  init_x, uint  init_y)
-{
-	uint score=0;
-	uint * my_highest = new uint[6];
-	fill(my_highest, my_highest + 6, 0);
-
-	for (int i = init_x; i < init_x + h; i++)   //Searching for max and min of RGB pixels
+	for (int i = init_x; i < init_x + h; i++)
 		for (int j = init_y; j < init_y + w; j = j + 4) {
 
-			if (out[i][j] > my_highest[0]) //max R
-				my_highest[0] = out[i][j];
+			if (out[i][j] > max_R)
+				max_R = out[i][j];
+			else if (out[i][j] < min_R)
+				min_R = out[i][j];
 
-			if (out[i][j] < my_highest[1]) //min R
-				my_highest[1] = out[i][j];
+			if (out[i][j + 1] > max_G)
+				max_G = out[i][j + 1];
+			else if (out[i][j] < min_G)
+				min_G = out[i][j + 1];
 
-			if (out[i][j+1] > my_highest[2]) //max G
-				my_highest[2] = out[i][j+1];
-
-			if (out[i][j+1] < my_highest[3]) //min G
-				my_highest[3] = out[i][j+1];
-
-			if (out[i][j+2] > my_highest[4])  //max B
-				my_highest[4] = out[i][j+2];
-
-			if (out[i][j+2] < my_highest[5])   //min B
-				my_highest[5] = out[i][j+2];
-
-			for (int k=0; k < 6; k++)
-			{
-				if (k % 2)
-					score -= my_highest[k];
-				else
-					score += my_highest[k];
-			}
-			
+			if (out[i][j + 2] > max_B)
+				max_B = out[i][j + 2];
+			else if (out[i][j + 2] < min_B)
+				min_B = out[i][j + 2];
 		}
 
-	delete my_highest;
-	return score;
+	return (max_R - min_R + max_G - min_G + max_B - min_B);
+
+}
+
+void Compresor::array_to_matrix(char array[], unsigned int array_length, char **matrix, unsigned int w) {
+	int fils = 0;		//filas
+	int cols = 0;		//columnas
+
+	for (int h = 0; h < array_length; cols++, h++) {
+
+		if ((h + 1) % w == 0){		//cambio de fila!!!
+			fils++;			//cambio de fila
+			cols = 0;		//reinicio las columnas.
+		}
+		
+		matrix[fils][cols] = array[h];
+	}
 }
